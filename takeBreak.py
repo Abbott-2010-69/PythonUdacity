a30a7ecf9817f19d1ab8e8db526bf5dd886d340d
Abbott-2010-69 <Avesh.p.singh@gmail.com>
2018-07-07 08:27:02 +0200

    Completed take a break program. It plays youtube video after 2 hours. Still in test phase need to change sleep value to 2 hours currently it waits 5 seconds

--- takeBreak.py
+++ takeBreak.py
@@ -1,4 +1,9 @@
 #this program will take a break every few hours
 import time
 import webbrowser
-
+total_breaks = 3
+break_count = 0
+while(break_count<total_breaks)
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=dQ4w9WgXCQ")
+    break_count = break_count + 1
