72ec83241ca490095b40f3b01392594e07ec2633
Abbott-2010-69 <Avesh.p.singh@gmail.com>
2018-07-07 09:48:56 +0200

    Added second project to rename files in a directory.

--- takeBreak.py
+++ takeBreak.py
@@ -3,7 +3,9 @@ import time
 import webbrowser
 total_breaks = 3
 break_count = 0
-while(break_count<total_breaks)
+print("this program started on "+time.ctime())
+while(break_count<total_breaks):
     time.sleep(5)
-    webbrowser.open("https://www.youtube.com/watch?v=dQ4w9WgXCQ")
+    webbrowser.open("https://www.youtube.com/watch?v=-qlJiGGvPUI")
     break_count = break_count + 1
+print("completed")
