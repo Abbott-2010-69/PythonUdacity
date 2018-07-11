1c49f303d585be79c3c0a99a29b897be4ce19668
Abbott-2010-69 <Avesh.p.singh@gmail.com>
2018-07-11 14:19:31 +0200

    Changed algorithm of pentagon to draw a regular pentagon with all interior angles equal to 108 degrees and all sides equal

--- RenameFiles.py
+++ RenameFiles.py
@@ -1,10 +1,11 @@
+import os
 def rename_files():
     #enter file path here
     file_list = os.listdir(r"C:\dest")
     saved_path = os.getcwd
     os.chdir(r"C:\dest")
     for file_name in file_list:
-        os.rename(file_name,file_name.translate(None,"0123456789")))
+        os.rename(file_name, file_name.translate(None, "0123456789"))
     os.chdir(saved_path)
 
 rename_files()
---
+++ drawShapes.py
@@ -0,0 +1,63 @@
+import turtle
+def draw_pentagram(some_turtle):
+    some_turtle.left(60)
+    some_turtle.forward(100)
+    some_turtle.right(120)
+    some_turtle.forward(100)
+    some_turtle.right(150)
+    some_turtle.forward(125)
+    some_turtle.right(150)
+    some_turtle.forward(110)
+    some_turtle.right(150)
+    some_turtle.forward(120)
+
+
+def draw_pentagon(some_turtle):
+    some_turtle.forward(100)
+    some_turtle.right(108)
+    some_turtle.forward(100)
+    some_turtle.right(108)
+    some_turtle.forward(100)
+    some_turtle.right(108)
+    some_turtle.forward(100)
+    some_turtle.right(108)
+    some_turtle.forward(100)
+    some_turtle.right(108)
+
+
+def draw_hexagon(some_turtle):
+    for i in range(1,7):
+        some_turtle.forward(100)
+        some_turtle.right(60)
+
+def draw_circle(some_turtle):
+    some_turtle.circle(100)
+
+def draw_triangle(some_turtle):
+    for i in range(1,4):
+        some_turtle.forward(100)
+        some_turtle.right(120)
+
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_shape():
+
+    window = turtle.Screen()
+    window.bgcolor("red")
+    mover = turtle.Turtle()
+    mover.shape("turtle")
+    mover.color("yellow")
+    draw_pentagram(mover)
+    for i in range(1,37):
+        #draw_square(mover)
+        #draw_triangle(mover)
+        #draw_circle(mover)
+        #draw_hexagon(mover)
+        #draw_pentagon(mover)
+        draw_pentagram(mover)
+        mover.right(10)
+    window.exitonclick()
+draw_shape()
